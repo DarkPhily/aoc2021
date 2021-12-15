fcc3aef9102a9d1ae5be1704402667dbfbf98857
darkphily <sweet-guy@hotmail.de>
2021-12-15 18:03:23 +0100

    code for day15

--- day15/src/day15.py
+++ day15/src/day15.py
@@ -1,42 +1,61 @@
 import numpy as np
-import timeit
+from pathfinding.core.grid import Grid
+from pathfinding.finder.dijkstra import DijkstraFinder
 
-start_time = timeit.default_timer()
 with open("../data/input.txt", "r") as input_file:
     array2d = [[int(digit) for digit in list(line.rstrip("\n"))] for line in input_file]
     chitons = np.array(array2d)
-    chitons_dict = {coordinates: int(chiton) for coordinates, chiton in np.ndenumerate(chitons)}
-
-
-def get_neighbours(x, y):
-    return filter(chitons_dict.get, [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)])
 
 
 def find_path(graph, start, end):
-    distances = {coordinates: float("inf") for coordinates, _ in np.ndenumerate(graph)}
-    visited = {coordinates: False for coordinates, _ in np.ndenumerate(graph)}
-    distances[start] = 0
-    while True:
-        shortest_distance = float("inf")
-        shortest_coordinate = -1
-        for key, value in np.ndenumerate(graph):
-            if distances[key] < shortest_distance and not visited[key]:
-                shortest_distance = distances[key]
-                shortest_coordinate = key
-        # print("Visiting node {} with current distance {}".format(shortest_coordinate, shortest_distance))
-        if shortest_coordinate == -1:
-            return distances[(end[0]-1, end[1]-1)]
-        for neighbour in get_neighbours(*shortest_coordinate):
-            if graph[neighbour] != 0 and distances[neighbour] > distances[shortest_coordinate] + graph[neighbour]:
-                distances[neighbour] = distances[shortest_coordinate] + graph[neighbour]
-                # print("Updating distance of node {} to {}".format(neighbour, distances[neighbour]))
-        visited[shortest_coordinate] = True
+    finder = DijkstraFinder()
+    start = grid.node(*start)
+    end = grid.node(end[0]-1, end[1]-1)
+    path, runs = finder.find_path(start, end, graph)
+    return path
+
+
+def get_full_map(graph):
+    map_0 = graph
+    map_1 = graph + 1
+    map_1 = np.where(map_1 == 10, 1, map_1)
+    map_2 = map_1 + 1
+    map_2 = np.where(map_2 == 10, 1, map_2)
+    map_3 = map_2 + 1
+    map_3 = np.where(map_3 == 10, 1, map_3)
+    map_4 = map_3 + 1
+    map_4 = np.where(map_4 == 10, 1, map_4)
+    map_5 = map_4 + 1
+    map_5 = np.where(map_5 == 10, 1, map_5)
+    map_6 = map_5 + 1
+    map_6 = np.where(map_6 == 10, 1, map_6)
+    map_7 = map_6 + 1
+    map_7 = np.where(map_7 == 10, 1, map_7)
+    map_8 = map_7 + 1
+    map_8 = np.where(map_8 == 10, 1, map_8)
+    row_1 = np.concatenate((map_0, map_1, map_2, map_3, map_4), axis=1)
+    row_2 = np.concatenate((map_1, map_2, map_3, map_4, map_5), axis=1)
+    row_3 = np.concatenate((map_2, map_3, map_4, map_5, map_6), axis=1)
+    row_4 = np.concatenate((map_3, map_4, map_5, map_6, map_7), axis=1)
+    row_5 = np.concatenate((map_4, map_5, map_6, map_7, map_8), axis=1)
+    return np.concatenate((row_1, row_2, row_3, row_4, row_5), axis=0)
+
+
+def count_weight(path, map, start):
+    return sum(map[tuple(reversed(point))] for point in path) - map[tuple(reversed(start))]
 
 
 # --->Part1
-print("Result 1: {}".format(find_path(chitons, (0, 0), chitons.shape)))
+grid = Grid(matrix=chitons)
+optimal_path = find_path(grid, (0, 0), chitons.shape)
+print("Result 1: {}".format(count_weight(optimal_path, chitons, (0, 0))))
 
 
+# --->Part2
+full_map = get_full_map(chitons)
+grid = Grid(matrix=full_map)
+optimal_path = find_path(grid, (0, 0), full_map.shape)
+print("Result 2: {}".format(count_weight(optimal_path, full_map, (0, 0))))
 
 
 
